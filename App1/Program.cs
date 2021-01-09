6671d20934a70e88ed990100dcb1fd96d768c8dd
Valik-Ok <66696606+Valik-Ok@users.noreply.github.com>
2021-01-09 14:10:23 +0300

    #3.02

--- App1/Program.cs
+++ App1/Program.cs
@@ -1,18 +1,34 @@
 ﻿using System;
 
-//#3
+//#3 Проверка < или > 
 namespace App1
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
+            double num;
+            Console.WriteLine("Сколько тебе лет?");
+            num = Convert.ToDouble (Console.ReadLine());//Ввод num
 
+            if (num > 10d)
+                Console.WriteLine("Ты старше 10");
+            else if (num < 5d)
+                Console.WriteLine("Меньше 5");
+            else
+                Console.WriteLine("else");
+            //Проверка значения а так же вывод num
+
+            String name = num == 20 ? "First" : "Second";
+            Console.WriteLine(name);
+            //Вывод name First или Second , в зависимости от значения num
+
+            Console.ReadKey();
         }
     }
 }
 
-//Второй урок.Канкулятор
+//#2 Второй урок.Канкулятор
 /*
 namespace App1
 {
@@ -35,7 +51,7 @@ namespace App1
 }
 */
 
-// Первый запуск
+//#1 Первый запуск
 /*
 Первый тест программы. Выводит камандную строку, и закрывает после нажатия кнопки.
 
