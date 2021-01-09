ac8d2c93e2b41c9efef0e39027154e2af6bcb4e3
Valik-Ok <66696606+Valik-Ok@users.noreply.github.com>
2021-01-09 13:17:41 +0300

    Добавьте файлы проекта.

---
+++ App1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+ //Второй урок.Канкулятор
+namespace App1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num, num_1;
+            //Ввод переменных
+            Console.WriteLine("Введите первое число:");
+            num = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("\nВведите второе число:");
+            num_1 = Convert.ToInt32(Console.ReadLine());
+            //Вывод
+            Console.WriteLine("Результат:" + (num + num_1).ToString());
+            Console.ReadKey();//Что бы консоль не закрылась сразу
+
+        }
+    }
+}
+// Первый запуск
+/*
+Первый тест программы. Выводит камандную строку, и закрывает после нажатия кнопки.
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, evrivan!"); // надпись в консоле 
+            Console.ReadKey();
+        }
+    }
+}
+*/
+
