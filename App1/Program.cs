637655e50df58244f0d4e8809a817891f6812a79
Valik-Ok <66696606+Valik-Ok@users.noreply.github.com>
2021-04-05 12:48:36 +0300

    #13.1

--- App1/Program.cs
+++ App1/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic; // Масивы: Список
 
-//#13 Структуры 
+//#13 Структуры ( Уменьшаная копия класов )
 namespace ProjectOne
 {
+    struct Book
+    {
+        public string title;
+        public string name;
+        public string author;
+    }
     class MainClass
     {
         public static void Main (string[] args)
         {
+            Book b;
+            b.author = "King";
             Console.ReadLine();
         }
     }
