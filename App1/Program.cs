46a089878d2acf91af29b7904898ef358bf05d9b
Valik-Ok <66696606+Valik-Ok@users.noreply.github.com>
2021-04-05 11:39:13 +0300

    #10.1

--- App1/Program.cs
+++ App1/Program.cs
@@ -2,13 +2,43 @@
 using System.Collections.Generic; // Масивы: Список
 
 
-//#10 
+//#10 Интерфейсы 
 namespace ProjectOne
 {
+    interface ISomeInterface // Интерфесы принето называть в начале ISome. (Не пишем модификаторы доступа)
+    {
+        int property { get; set; }
+        void Print();
+    }
+
+    class Test_1 : ISomeInterface
+    {
+        public int property { get; set; }
+        public void Print() { Console.WriteLine("Some text"); }
+    }
+    class Test_2 : ISomeInterface
+    {
+        public int property { get; set; }
+        public void Print() { Console.WriteLine("This is text_2"); }
+    }
+
     class Mainclass
     {
         public static void Main (string[]args)
         {
+            Test_1 test_1 = new Test_1();
+            Test_2 test_2 = new Test_2();
+
+            test_1.property = 12;
+            test_1.Print();
+            Console.WriteLine(test_1.property);
+
+            Console.WriteLine();
+
+            test_2.property = 15;
+            test_2.Print();
+            Console.WriteLine(test_2.property);
+
             Console.ReadLine();
         }
     } 
